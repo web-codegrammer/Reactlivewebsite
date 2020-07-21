bbc0607ed136a19d862d49b0bd959751fd291ede
Devanshu Vashishtha <sharmadevanshu452@gmail.com>
2020-07-21 19:11:52 +0530

    Updated Reactlive Website

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="w-100 bg-light text-center">
+        <p>© 2020 <strong> WebCodeGrammer </strong>. All Rights Reserved | Designed & Developed by <strong> Devanshu Vashishtha </strong> </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
